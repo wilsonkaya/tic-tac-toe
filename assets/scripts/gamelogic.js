9d580647744cd936349b77c2c2c7e8a2acfe5181
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-01-25 14:51:27 -0500

    Define the turn of the player

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -1,69 +1,55 @@
 "use strict";
-
-"use strict";
 let finalResult = ['','','','','','','','',''];
-let currentPlayer = "";
-let playerOne = "x";
-let playerTwo = "o";
+let currentPlayer = "x";
 
-let a = finalResult[0];
-let b = finalResult[1];
-let c = finalResult[2];
-let d = finalResult[3];
-let e = finalResult[4];
-let f = finalResult[5];
-let g = finalResult[6];
-let h = finalResult[7];
-let i = finalResult[8];
-let countOfClick = 0;
-
-
-
-
-const refresh = function(){
-   a = [];
-   b = [];
-   c = [];
-   d = [];
-   e = [];
-   f = [];
-   g = [];
-   h = [];
-   i = [];
-   countOfClick = 0;
-};
+let resettingArray = ['','','','','','','','',''];
 
 const decidePlayer = function(){
-  if(finalResult.length % 2 === 0){
-    currentPlayer = playerOne
+ let XnO = 0;
+  for(let i = 0; i < finalResult.length; i++){
+    if(finalResult[i] === 'x' || finalResult[i] === 'o'){
+     XnO ++;
+   }
   }
-}
+ if(XnO % 2 === 0){
+   currentPlayer = 'x';
+ }else if (XnO % 2 ){
+   currentPlayer = 'o';
+ }
+ if(XnO === 9){
+   finalResult = resettingArray;
+   currentPlayer = 'x';
+ }
+
+};
 
 
- const horizontalResult = function(){
+
+ const horizontalResult = function( ){
    if (finalResult[0] === finalResult[1] && finalResult[1] === finalResult[2] ) {
+     console.log("x wins");
      return finalResult[0] + " wins";
    }else if(finalResult[3] === finalResult[4] && finalResult[4] === finalResult[5]) {
      return finalResult[3] + "wins";
    }else if(finalResult[6] === finalResult[7] && finalResult[7] === finalResult[8]) {
-     return d[0] + " wins";
+     return finalResult[6] + " wins";
    }
   //  return false;
  };
 
 
- const verticalResult = function(){
+ const verticalResult = function( ){
    if (finalResult[0] === finalResult[3] && finalResult[3] === finalResult[6] ) {
      return finalResult[0] + "wins";
    }else if(finalResult[1] === finalResult[4] && finalResult[4] === finalResult[7]) {
      return finalResult[1] + "wins";
-   }else if(finalResult[2] === f[0] && f[0] === i[0]) {
+   }else if(finalResult[2] === finalResult[5] && finalResult[5] === finalResult[8]) {
      return finalResult[2] + " wins";
    }
   //  return false;
  };
 
- const diagonalResult = function(){
+ const diagonalResult = function( ){
    if (finalResult[0] === finalResult[4] && finalResult[4] === finalResult[8] ) {
      return finalResult[0] + " wins";
    }else if(finalResult[2] === finalResult[4] && finalResult[4] === finalResult[6]) {
@@ -73,20 +59,23 @@ const decidePlayer = function(){
  };
 
 
+ const allFunctions = function() {
+   horizontalResult();
+   verticalResult();
+   diagonalResult();
 
 
+ };
 
+ //function to player
+ const gamePlay = function(number){
+   finalResult[number] = currentPlayer;
+  console.log(finalResult);
+  decidePlayer();
+  //  decidePlayer();
+   return allFunctions(finalResult);
+ };
 
-//
-//  const finalResult = function(){
-//    horizontalResult();
-//    verticalResult();
-//    diagonalResult()
-// };
-
-finalResult();
 module.export = {
-  horizontalResult,
-  verticalResult,
-  diagonalResult
+ gamePlay,
 };
