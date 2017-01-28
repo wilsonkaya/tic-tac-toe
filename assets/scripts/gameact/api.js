6ee16d17df85564e8a5c1d33f2f49b763d47b545
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-01-28 13:04:38 -0500

    Edit update game, solve bug in url

--- assets/scripts/gameact/api.js
+++ assets/scripts/gameact/api.js
@@ -3,6 +3,17 @@
 const config = require("../config");
 const store = require("../store");
 
+// const index = function(){
+//   return $.ajax({
+//     url: config.apiOrigin + '/games',
+//     method: 'GET',
+//     headers: {
+//       Authorization:`Token token=${store.user.token}`
+//     }
+//   });
+// };
+
+
 const createNewGAme = function(){
 return $.ajax({
   url: config.apiOrigin + '/games',
@@ -27,7 +38,7 @@ const showUserGame = function(id){
 const updateUserGame = function(data){
   console.log("updateUserGame Data: ", data);
   return $.ajax({
-    url: config.apiOrigin + '/games/' + `${store.user.id}`,
+    url: config.apiOrigin + '/games/' + `${store.game.id}`,
     method: "PATCH",
     headers:{
       Authorization:`Token token=${store.user.token}`
@@ -49,4 +60,5 @@ module.exports = {
   createNewGAme,
   showUserGame,
   updateUserGame,
+  // index
 };
