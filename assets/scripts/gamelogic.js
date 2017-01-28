f13a28d20f6aade2875d8cf39afb4d05c24d68e2
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-01-28 15:59:03 -0500

    Add if statement at sendMove to overcome bug

--- assets/scripts/gamelogic.js
+++ assets/scripts/gamelogic.js
@@ -26,11 +26,12 @@ const decidePlayer = function(number){
    finalResult = resettingArray;
    currentPlayer = 'x';
  }
-
+findWinner(number);
+sendMove(number);
 };
 
 //find winner
- const findWinner = function( ){
+ const findWinner = function(){
    if ((finalResult[0] === "x" && finalResult[1] === "x" && finalResult[2] === "x") ||
       (finalResult[3] === "x" && finalResult[4] === "x" && finalResult[5] === "x") ||
       (finalResult[6] === "x" && finalResult[7] === "x" && finalResult[8] === "x") ||
@@ -41,6 +42,7 @@ const decidePlayer = function(number){
       (finalResult[2] === "x" && finalResult[4] === "x" && finalResult[6] === "x")) {
         finalResult = resettingArray;
         resettingGame = true;
+
         console.log("x wins");
       } else if ((finalResult[0] === "o" && finalResult[1] === "o" && finalResult[2] === "o") ||
          (finalResult[3] === "o" && finalResult[4] === "o" && finalResult[5] === "o") ||
@@ -53,7 +55,9 @@ const decidePlayer = function(number){
          finalResult = resettingArray;
          resettingGame = true;
          console.log("o wins");
-         }
+       }
+
+
 
 };
 
@@ -70,7 +74,11 @@ let singleUpdate = {
 
 
 let sendMove = function (number){
-  singleUpdate['game']['cell']['value'] = currentPlayer;
+  if(currentPlayer === "x"){
+    singleUpdate['game']['cell']['value'] = "o"
+  } else{
+    singleUpdate['game']['cell']['value'] = "x"
+  }
   singleUpdate['game']['cell']['index'] = parseInt(number.id);
   singleUpdate['game']['over'] = resettingGame;
 // console.log(singleUpdate)
@@ -78,15 +86,17 @@ let sendMove = function (number){
 };
 
  //function to player // number is the div att of html
+
  const gamePlay = function(number){
    if (resettingGame === false){
      if($(number).text() !== "x" && $(number).text() !== "o"){
        $(number).text(currentPlayer);
      }
 
-    sendMove(number);
+    // sendMove(number);
     decidePlayer(number);
-    findWinner(number);
+
+
 
    }
 
